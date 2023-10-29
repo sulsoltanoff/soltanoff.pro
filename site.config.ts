820d9b9b254c7fd57e847c79ebd360c281ee4f1b
Sultan Soltanov <sulsoltanoff@icloud.com>
2023-10-29 16:22:55 +0500

    Start project

---
+++ site.config.ts
@@ -0,0 +1,65 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: 'b86bf1db3e994471979bab97aec83243',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Soltanoff',
+  domain: 'sulsoltanoff.notion.site',
+  author: 'Sultan Soltanov',
+
+  // open graph metadata (optional)
+  description: 'My personal blog in world wide web',
+
+  // social usernames (optional)
+  twitter: 'sulsoltanoff',
+  github: 'sulsoltanoff',
+  linkedin: 'sulsoltanoff',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '0c50a1253ebb42cca4182555d27af6ab'
+    },
+    {
+      title: 'Contact',
+      pageId: '5000657397704016af199c10a455ebfe'
+    }
+  ]
+})
