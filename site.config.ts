b914b9f382ecb85328ebd8e4abf8bea3ecfad837
Sultan Soltanov <sulsoltanoff@icloud.com>
2023-10-29 20:13:08 +0500

    Change spase

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'db3d6c2fd5e54baaafc103befd51b1be',
+  rootNotionPageId: 'b86bf1db3e994471979bab97aec83243',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
